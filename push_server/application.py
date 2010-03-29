05022f9f2196fdff05f1fe307121f58a74934e36
Sean O'Connor <sean@seanoc.com>
2010-03-28 22:37:55 -0400

    Added support for channels.

--- push_server/application.py
+++ push_server/application.py
@@ -7,6 +7,8 @@ from dateutil.tz import tzutc
 from werkzeug import Request, Response, ClosingIterator, Local, LocalManager
 from werkzeug.routing import Map, Rule
 
+from utils import MultiValueDict
+
 local = Local()
 local_manager = LocalManager([local])
 application = local('application')
@@ -23,21 +25,28 @@ class PushServer(object):
             'subscribe': self.on_subscribe,
             'update': self.on_update,
         }
-        self.updates = []
+        self.updates = MultiValueDict()
         self.max_updates = 5
 
 
     def on_subscribe(self, request):
         last_modified = local.last_modified = request.headers.get('last_modified', None)
-        if last_modified is not None:
-            last_modified = parse_timestr(last_modified)
+        channel = request.args.get('channel', None)
         
-        local.next_update = next_update = self.get_next_update(last_modified)
-        while (next_update is None):
-            time.sleep(1)
-            next_update = self.get_next_update(last_modified)
+        if channel is not None:
+            if last_modified is not None:
+                last_modified = parse_timestr(last_modified)
         
-        return self.send_update(next_update)
+            local.next_update = next_update = self.get_next_update(channel, last_modified)
+            while (next_update is None):
+                time.sleep(1)
+                next_update = self.get_next_update(channel, last_modified)
+        
+            response = self.send_update(next_update)
+        else:
+            response = Response(response="A channel get param must be passed.", status=400)
+        
+        return response
         
     def send_update(self, update):
         response = Response(
@@ -49,28 +58,35 @@ class PushServer(object):
         return response
         
                 
-    def get_next_update(self, last_modified):
+    def get_next_update(self, channel, last_modified):
         next_update = None
-        for update in self.updates:
+        for update in self.updates.getlist(channel):
             if last_modified is None or update['published_on'] > last_modified:
                 next_update = update
                 break;
                 
         return next_update
     
-    def on_update(self, request):
+    def on_update(self, request):    
+        channel = request.args.get('channel', None)
+        
         if request.method == 'POST' or request.method == 'PUT':
-            update = {
-                'body': request.data,
-                'content_type': request.content_type,
-                'published_on': datetime.now(tzutc()),
-            }
+            if channel is not None:
+                update = {
+                    'body': request.data,
+                    'content_type': request.content_type,
+                    'published_on': datetime.now(tzutc()),
+                }
+                
+                channel_updates = self.updates.getlist(channel)
+                if len(channel_updates) >= self.max_updates:
+                    channel_updates = channel_updates[-(self.max_updates - 1):]
         
-            if len(self.updates) >= self.max_updates:
-                self.updates = self.updates[-(self.max_updates - 1):]
-        
-            self.updates.append(update)
-            response = Response(response="Update Accepted", status=201)
+                channel_updates.append(update)
+                self.updates.setlist(channel, channel_updates)
+                response = Response(response="Update Accepted", status=201)
+            else:
+                response = Response(response="A channel get param must be passed.", status=400)
         else:
             response = Response(response="Method not allowed", status=405)
             
